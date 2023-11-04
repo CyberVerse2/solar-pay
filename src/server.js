81b6e63cd36b0829a75de987c48a2fb5dae96cf3
ejioforcelestine <ejioforcelestine77@gmail.com>
2023-11-04 04:53:09 +0100

    allowed request from all clients

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const appName = ENVIRONMENT.APP.NAME;
  * App Security
  */
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({limit: "50mb", extended: true}));
 app.disable("x-powered-by");
